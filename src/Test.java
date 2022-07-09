60d080d8917a9b90d6157598e46de7af2e3b7d8b
zsb <1326739216@qq.com>
2022-07-09 15:27:40 +0800

    init test

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author z's'b
+ * @version 1.0
+ * @date 2022/7/9 13:39
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
